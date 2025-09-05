14b08d17b9983c38d74e04c5d4bb711fbc19f5ee
damek <damek@users.noreply.github.com>
2025-09-05 11:17:29 -0400

    update look_at_cc.sh to download WARC and WET files into data/CC directory

--- cs336_data/look_at_cc.sh
+++ cs336_data/look_at_cc.sh
@@ -1,8 +1,8 @@
 # Download WARC and WET files to cs336_data/assets
 # name them example.warc.gz and example.warc.wet.gz
 
-mkdir -p assets
-cd assets    
+mkdir -p data/CC
+cd data/CC    
 wget https://data.commoncrawl.org/crawl-data/CC-MAIN-2025-18/segments/1744889135610.12/warc/CC-MAIN-20250417135010-20250417165010-00065.warc.gz
 mv CC-MAIN-20250417135010-20250417165010-00065.warc.gz example.warc.gz
 wget https://data.commoncrawl.org/crawl-data/CC-MAIN-2025-18/segments/1744889135610.12/wet/CC-MAIN-20250417135010-20250417165010-00065.warc.wet.gz
