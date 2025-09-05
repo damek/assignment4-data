e9d9f295eb1222cdf02db6a4b8dc2507bc1c4176
damek <damek@users.noreply.github.com>
2025-09-05 18:52:15 -0400

    s

--- cs336_data/gopher_quality_filters.py
+++ cs336_data/gopher_quality_filters.py
@@ -7,11 +7,12 @@ def is_too_little_or_too_many_words(tokens: list[str]):
     return len(tokens) < 50 or len(tokens) > 100000
 
 def is_average_word_length_less_than_3(tokens: list[str]):
-    return sum(len(word) for word in tokens) / len(tokens) < 3
+    print("average word length: ", sum(len(word) for word in tokens) / len(tokens))
+    return (sum(len(word) for word in tokens) / len(tokens)) < 3
 
 def is_more_than_30_percent_lines_ending_with_ellipsis(lines: list[str]):
     return sum(line.endswith("...") for line in lines) > 0.3 * len(lines)
-
+    
 def is_less_than_80_percent_words_with_at_least_one_alphabetic_character(tokens: list[str]):
     return sum(1 for word in tokens if re.search(r'[a-zA-Z]', word)) < 0.8 * len(tokens)
 
