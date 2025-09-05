edb1103280b0dbaf46933d909adda8d32ec2558e
damek <damek@users.noreply.github.com>
2025-09-05 15:08:25 -0400

    s

--- cs336_data/harmful_content.py
+++ cs336_data/harmful_content.py
@@ -5,7 +5,7 @@ def _model_NSFW():
     if not os.path.exists(os.path.join(os.path.dirname(os.path.abspath(__file__)), "models/jigsaw_fasttext_bigrams_nsfw_final.bin")):
         os.system("bash harmful_content.sh")
     return fasttext.load_model(os.path.join(os.path.dirname(os.path.abspath(__file__)), "models/jigsaw_fasttext_bigrams_nsfw_final.bin"))
-d
+
 def _model_Hatespeech():
     if not os.path.exists(os.path.join(os.path.dirname(os.path.abspath(__file__)), "models/jigsaw_fasttext_bigrams_hatespeech_final.bin")):
         os.system("bash harmful_content.sh")
