7187e01fe59cdbe8bb7942066617c9b771479f33
damek <damek@users.noreply.github.com>
2025-09-05 11:15:23 -0400

    s

--- cs336_data/look_at_cc.sh
+++ cs336_data/look_at_cc.sh
@@ -4,6 +4,6 @@
 mkdir -p assets
 cd assets    
 wget https://data.commoncrawl.org/crawl-data/CC-MAIN-2025-18/segments/1744889135610.12/warc/CC-MAIN-20250417135010-20250417165010-00065.warc.gz
-mv warc/CC-MAIN-20250417135010-20250417165010-00065.warc.gz example.warc.gz
+mv CC-MAIN-20250417135010-20250417165010-00065.warc.gz example.warc.gz
 wget https://data.commoncrawl.org/crawl-data/CC-MAIN-2025-18/segments/1744889135610.12/wet/CC-MAIN-20250417135010-20250417165010-00065.warc.wet.gz
-mv wet/CC-MAIN-20250417135010-20250417165010-00065.warc.wet.gz example.warc.wet.gz
+mv CC-MAIN-20250417135010-20250417165010-00065.warc.wet.gz example.warc.wet.gz
