a4c64a5570cf8d8798cbc3aed0c2babbcb584ec3
damek <damek@users.noreply.github.com>
2025-09-05 12:45:42 -0400

    try

--- cs336_data/language_identification.py
+++ cs336_data/language_identification.py
@@ -4,8 +4,15 @@ import fasttext
 def _model():
     return fasttext.load_model("models/lid.176.bin")
 
-def identify_language(text: str) -> str:
+def identify_language_str(text: str) -> str:
     return _model().predict(text)
 
+def identify_language_bytes(text: bytes) -> str:
+    return _model().predict(text)
+
+def identify_language_file(file: str) -> str:
+    with open(file, "rb") as f:
+        return _model().predict(f.read())
+
 if __name__ == "__main__":
-    print(identify_language("Hello, world!"))
+    print(identify_language_bytes(b"Hello, world!"))
