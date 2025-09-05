bb0c4cc6a2a3bbac6024ed019973e99713b16fb7
damek <damek@users.noreply.github.com>
2025-09-05 15:16:05 -0400

    s

--- cs336_data/harmful_content.py
+++ cs336_data/harmful_content.py
@@ -1,5 +1,9 @@
 import fasttext
 import os
+import random
+from fastwarc.archive import ArchiveIterator
+from fastwarc.warc import WarcRecordType
+from cs336_data.extract_text import extract_text_from_html_bytes
 
 def _model_NSFW():
     if not os.path.exists(os.path.join(os.path.dirname(os.path.abspath(__file__)), "models/jigsaw_fasttext_bigrams_nsfw_final.bin")):
@@ -23,6 +27,41 @@ def classify_hatespeech(text: str):
     predicted_class, score = HATESPEECH_MODEL.predict(text)
     return predicted_class[0].split("__label__")[1], score[0]
 
+
+def extract_warc_and_detect_harmful_content(nb_entries: int = 20) -> list[str]:
+    nsfw_labels = []
+    hatespeech_labels = []
+    texts = []    
+    # get path
+    file = os.path.join(os.path.dirname(os.path.abspath(__file__)), "data/CC/example.warc.gz")
+    if not os.path.exists(file):
+        os.system("bash look_at_cc.sh")
+    if not os.path.exists("models/jigsaw_fasttext_bigrams_hatespeech_final.bin") or not os.path.exists("models/jigsaw_fasttext_bigrams_nsfw_final.bin"):
+        os.system("bash harmful_content.sh")
+    # We're going to pick 20 entries at random from the first 1000 entries 
+    entries = random.sample(range(10000), nb_entries)
+    with open(file, "rb") as f:
+        for i, record in enumerate(ArchiveIterator(f, record_types=WarcRecordType.response)):
+            if i in entries:
+                text = extract_text.extract_text_from_html_bytes(record.reader.read())
+                nsfw_labels.append(classify_nsfw(text))
+                hatespeech_labels.append(classify_hatespeech(text))
+                texts.append(text)
+            if len(texts) == nb_entries:
+                break
+    return nsfw_labels, hatespeech_labels, texts
 if __name__ == "__main__":
-    print(classify_nsfw("OMFG"))
-    print(classify_hatespeech("This is a test"))
+
+    # first print then save to outputs/harmful_content.txt
+    nsfw_labels, hatespeech_labels, texts = extract_warc_and_detect_harmful_content(20)
+    for nsfw_label, hatespeech_label, text in zip(nsfw_labels, hatespeech_labels, texts):
+        print("--------------------------------")
+        print("NSFW LABEL: ", nsfw_label[0], "HATESPEECH LABEL: ", hatespeech_label[0])
+        print("text: ", text)
+        print("--------------------------------")
+    with open("outputs/harmful_content.txt", "w") as f:
+        for nsfw_label, hatespeech_label, text in zip(nsfw_labels, hatespeech_labels, texts):
+            f.write("--------------------------------\n")
+            f.write("NSFW LABEL: " + nsfw_label[0] + " HATESPEECH LABEL: " + hatespeech_label[0] + "\n")
+            f.write("text: " + text + "\n")
+            f.write("--------------------------------\n")
---
+++ cs336_data/outputs/harmful_content.md
@@ -0,0 +1,55 @@
+# Problem (harmful_content): 6 points
+
+Main code file: [harmful_content.py](../harmful_content.py)
+
+## Question 1
+
+Write a function to detect NSFW content.
+Deliverable: A function that labels a given string as containing NSFW content or not, returning
+a pair containing both the label and a confidence score. Implement the adapter
+[run_classify_nsfw] and make sure it passes
+uv run pytest -k test_classify_nsfw. Note that this test is just a sanity check, taken from
+the Jigsaw dataset, but by no means asserts that your classifier is accurate, which you should
+validate.
+
+**Answer:** Passed
+
+How to run: 
+```bash
+uv run pytest -k test_classify_nsfw
+```
+
+## Question 2
+
+Write a function to detect toxic speech.
+Deliverable: A function that labels a given string as consisting of toxic speech or not, returning
+a pair containing both the label and a confidence score. Implement the adapter
+[run_classify_toxic_speech] and make sure it passes
+uv run pytest -k test_classify_toxic_speech. Again, this test is just a sanity check, also
+taken from Jigsaw.
+
+**Answer:** Passed
+
+How to run: 
+```bash
+uv run pytest -k test_classify_toxic_speech
+```
+
+## Question 3
+
+What problems do you think might arise downstream in a language model when these filters are
+applied to create the training set? How might you mitigate these issues?
+Deliverable: A 2-5 sentence response.
+
+**Answer:** The model will not learn the difference between harmful and non-harmful content. It also won't be able to interact with harmful content reliably. It might also parrot harmful content back because of this. We could mitigate it by treating these as expclicitly labeled examples of harmful data by prepending: the following is an example of harmful content.
+
+## Question 4
+
+Run your harmful content filters on text extracted from the WARC files (via your previously-
+implemente  d text extraction function). Look through 20 random examples and compare the
+classifier predictions to your own judgments. Report any classifier errors. What fraction of
+documents are harmful? Based on your observations, what would be suitable classifier confidence
+threshold(s) to use in filtering?
+Deliverable: A 2-5 sentence response.
+
+**Answer:**
