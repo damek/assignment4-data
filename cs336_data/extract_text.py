85890bab0222005fa18a9cc327a1446f7387a08d
damek <damek@users.noreply.github.com>
2025-09-05 12:18:53 -0400

    s

--- cs336_data/extract_text.py
+++ cs336_data/extract_text.py
@@ -1,6 +1,7 @@
 from fastwarc.warc import ArchiveIterator, WarcRecordType
 from resiliparse.parse.encoding import detect_encoding, bytes_to_str
 from resiliparse.extract.html2text import extract_plain_text
+from argparse import ArgumentParser
 
 def extract_text_from_html_bytes(html_bytes: bytes) -> str:
     if not html_bytes:
@@ -22,12 +23,18 @@ def extract_text_from_html_bytes(html_bytes: bytes) -> str:
     # extract text
     return extract_plain_text(html_str)
 
-def extract_warc_file(warc_file: str) -> str:
-    # use fastwarc to first entry of warc file, extract text from html, and then print to terminal
-    text = ""
+def extract_warc_file(warc_file: str, nb_of_entries: int = 1) -> str:
+    # use fastwarc to first entry of warc file, extract text from html, and then return a list of entries
+    text = []
     with open(warc_file, "rb") as f:
         for record in ArchiveIterator(f):
             if record.type == WarcRecordType.RESPONSE:
-                text = extract_text_from_html_bytes(record.content)
-                break
+                text.append(extract_text_from_html_bytes(record.content))
+                if len(text) == nb_of_entries:
+                    break
     return text
+
+def hard_coded_extract_warc_file():
+    warc_file = "data/CC/example.warc.gz"
+    text = extract_warc_file(warc_file)
+    print(text)
