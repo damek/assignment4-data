f2bbc0836f49aba9eacea2d3e1b08a341ff82470
damek <damek@users.noreply.github.com>
2025-09-05 12:49:03 -0400

    Yes

--- cs336_data/language_identification.py
+++ cs336_data/language_identification.py
@@ -5,7 +5,9 @@ def _model():
     return fasttext.load_model("models/lid.176.bin")
 
 def identify_language_str(text: str) -> str:
-    return _model().predict(text)
+    predicted_language, score = _model().predict(text)
+    print(predicted_language, score)
+    return predicted_language[0], score[0]
 
 def identify_language_file(file: str) -> str:
     with open(file, "rb") as f:
