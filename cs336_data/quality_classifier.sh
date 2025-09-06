6fb86abaed9db77515ecd96b37f5abcea1f2e719
damek <damek@users.noreply.github.com>
2025-09-05 21:24:54 -0400

    s

--- cs336_data/quality_classifier.sh
+++ cs336_data/quality_classifier.sh
@@ -3,7 +3,7 @@
 # download classifier to the data/wiki_links
 URL="https://nlp.stanford.edu/data/nfliu/cs336-spring-2024/assignment4/enwiki-20240420-extracted_urls.txt.gz"
 
-mkdir -p data
+mkdir -p data/wiki
 [ -f data/wiki/links.txt.gz ] || wget -O data/wiki/links.txt.gz "$URL"
 
 if [ ! -f data/wiki/links.sample.txt.gz ]; then
